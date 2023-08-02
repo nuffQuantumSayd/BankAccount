5c600e85778f69efa39bfb0ed3f38c995ebb8799
nuffQuantumSayd <aaronkornish.ak@gmail.com>
2023-08-02 08:40:57 -0700

    Added some test code

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BankAccount;
+
+Account myAccount = new("Joe O");
+myAccount.Deposit(500);
