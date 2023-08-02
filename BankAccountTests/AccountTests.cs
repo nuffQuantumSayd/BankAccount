eb4b65dce2712342520c78e3586afe97fae732a3
nuffQuantumSayd <aaronkornish.ak@gmail.com>
2023-08-02 08:21:18 -0700

    Added withdraw test stubs

--- BankAccountTests/AccountTests.cs
+++ BankAccountTests/AccountTests.cs
@@ -79,7 +79,27 @@ namespace BankAccount.Tests
 
             //Assert
             Assert.AreEqual(expectedBalance, actualBalance);
+        }
 
+        [TestMethod]
+        public void Withdraw_PositiveAmount_ReturnsUpdatedBalance()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [DataRow(0)]
+        [DataRow(-.01)]
+        [DataRow(-1000)]
+        public void Withdraw_ZeroOrLess_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Withdraw_MoreThanAvailableBalance_ThrowsArgumentException()
+        {
+            Assert.Fail();
         }
     }
 }
